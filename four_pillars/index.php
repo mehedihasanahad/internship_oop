2afe9fd8e307f910770920e5b025bb84117cd1f8
Mehedi Hasan Ahad <ahad.egov@gmail.com>
2024-08-21 17:54:33 +0600

    four_pillars updated

---
+++ four_pillars/index.php
@@ -0,0 +1,14 @@
+<?php
+
+// The four pillars of Object-Oriented Programming (OOPS) are:
+
+// 1) Abstraction
+// 2) Encapsulation
+// 3) Inheritance
+// 4) Polymorphism
+
+
+require('encapsulation.php');
+// require('inheritance.php');
+// require('abstraction.php');
+// require('polymorphism.php');
--- index.php
+++ index.php
@@ -1,14 +1,11 @@
 <?php
 //require('class_and_object.php');
 
-//require('four_pillars/encapsulation.php');
-//require('four_pillars/inheritance.php');
-// require('four_pillars/abstraction.php');
-// require('four_pillars/polymorphism.php');
+require('four_pillars');
 
 //require('access_modifier.php');
 // require('interface.php');
 // require('trait.php');
 // require('static.php');
 //  require('namespace');
-require('magic_methods');
+// require('magic_methods');
